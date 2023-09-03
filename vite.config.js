0e5854cfa864b4da4ec923bf84c1e4e2c4c9bda3
Mayur Chopra <mayur5c@live.in>
2023-09-03 23:16:59 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-base:"/frontend-4-module-test-august"
+base:"/frontend-4-module-test-august/"
 export default defineConfig({
   plugins: [react()],
 })
